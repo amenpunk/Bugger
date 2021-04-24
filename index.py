f9e4aa00d6c13ceb3a23d4f00c1c3e62495b517d
amenpunk <ondasycircuitos@gmail.com>
2021-04-23 18:38:18 -0600

    pre config

---
+++ index.py
@@ -0,0 +1,36 @@
+import time
+
+departamentos = {
+    1 : "Contabilidad", 2 : "contabilidad", 3 : "presupuestos", 4 : "estadística", 5 : "estadistica",
+    6 : "", 7 : "ventas", 8 : ".", 9 : "rrhh", 10 : "recuros humanos", 11 : "IT", 12 : 'it', 13 : 'informatica',
+    12 : "IT",13 : 'papeles', 14 : 'general', 15 : 'actas', 16 : 'permisos', 17 : 'gestiones', 18 : "gerencia",
+    19 : 'administracion', 20 : 'compras', 21 : 'planificacion', 22 : "cobros", 23 : 'finanzas publicas', 24 : "presupuestos",
+    25 : 'informática', 26 : 'pedidos', 27 : "no se", 28 : 'niguno'
+}
+
+multiple = {
+    1 : "Sí",
+    2 : "No"
+}
+
+dispositivo = {
+    1 :  "Teléfono móvil",
+    2 :  "Computador"
+}
+
+motivos = {
+    1 : "No tengo conocimiento", 2 : "El proceso", 3 : "No ha pasado", 4 : "No se le ha puesto el interes debido",
+    5 : "", 6 : ".", 7 : "Información", 8 : "informacion", 9 : 'desconocimiento', 10 : 'Desconocimiento del Tema' , 11 : 'no estoy familiarizado',
+    12 : 'el sistema que se utiliza', 13 : "Desconocimiento" , 14 : 'oportunidades', 15 : 'factor economico', 17 : 'presupuesto', 18 : 'motivos monetarios'
+}
+
+
+
+def void():
+    while(True):
+        time.sleep(10)
+        print("hola")
+
+
+
+void();
